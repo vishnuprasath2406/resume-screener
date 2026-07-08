d057968d11ac50f1dd6a2071766900c9da24e34f
vishnuprasath2406 <vishnusuresh2406@gmail.com>
2026-07-08 21:16:14 +0530

    Add deployment config for Render (Procfile, gunicorn, dynamic port)

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ def analyze():
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
